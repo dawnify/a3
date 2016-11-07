8ae832ae5664fceb20f16787e8a3aa3d2360ba18
dawnify <Vinny@Vincents-MBP.local>
2016-11-07 14:00:31 -0500

    a3
    assignment

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+<head>
+  <meta charset = "utf-8">
+  <title>Games and Apps</title>
+  <?php
+  echo '<link href="style4.css" rel="stylesheet">';
+</head>
+<body>
+  <div class="content">
+<h1> What my friendlist played this week</h1>
+
+<br>
+  <a href="games.php"><img class="game" src="images/xbox.png"></a>
+<br>
+
+</div>
+</body>
+
+</html>
